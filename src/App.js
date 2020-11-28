d594af6b18f9d2bec1c62189773be0df948c1a98
Doyun <y0unn@hanmail.net>
2020-11-28 16:14:04 +0900

    Create Movie App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Move";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    };
+  }
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  componentDidUpdate() {
+    console.log("update");
+  }
+
+  render() {
+    console.log(this.state);
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((item) => (
+              <Movie
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                year={item.year}
+                summary={item.summary}
+                poster={item.medium_cover_image}
+                genres={item.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
