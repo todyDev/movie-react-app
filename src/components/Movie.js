fe476da008be2213925b81f2b9d5b3004dfc8f31
Doyun <y0unn@hanmail.net>
2020-12-08 21:34:54 +0900

    Fix the main page to look good

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import NoImage from "./css/no_image.png";
 import "./css/Movie.css";
 
 function Movie({ id, title, year, summary, poster, genres }) {
@@ -18,12 +19,12 @@ function Movie({ id, title, year, summary, poster, genres }) {
           }
         }}
       >
-        <img src={poster.indexOf("|") > -1 ? poster.slice(0, poster.indexOf("|")) : poster} alt={title} title={title} />
+        <img src={poster.indexOf("|") > -1 ? poster.slice(0, poster.indexOf("|")) : poster || NoImage} alt={title} title={title} />
         <div className="movie__data">
           <h3 className="movie__title">{title}</h3>
           <h5 className="movie__year">{year}</h5>
           <p className="movie__genres">{genres}</p>
-          <p className="movie__summary">{summary.slice(0, 140)}...</p>
+          <p className="movie__summary">{summary.length > 140 ? `${summary.slice(0, 140)}...` : summary}</p>
         </div>
       </Link>
     </div>
